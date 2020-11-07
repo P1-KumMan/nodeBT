e52f9ff44727a1b4ad7b798d1472d0815d2e12d4
P1-KumMan <adithan92014@gmail.com>
2020-11-07 08:22:05 +0530

    db schema added

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// const morgan = require("morgan");
+const morgan = require("morgan");
 
-// const postRoutes = require("./routes/post");
-
-// const MyMiddle = (req, res, next) => {
-//   console.log("middleware applied!!");
-//   next();
-// };
-
-// app.use(MyMiddle);
-
-// app.use(morgan("dev"));
-
-// app.use("/", postRoutes);
-
-// const port = 8080;
-
-// app.listen(port, () => {
-//   console.log(`A node.js app is  listening to port:${port}`);
-// });
-// import mongoose
+const postRoutes = require("./routes/post");
 const mongoose = require("mongoose");
 // load env variables
 const dotenv = require("dotenv");
 dotenv.config();
+const MyMiddle = (req, res, next) => {
+  console.log("middleware applied!!");
+  next();
+};
+
+app.use(MyMiddle);
+
+app.use(morgan("dev"));
+
+app.use("/", postRoutes);
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`A node.js app is  listening to port:${port}`);
+});
+// import mongoose
 
 //db connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+  )
   .then(() => console.log("DB Connected"));
 
 mongoose.connection.on("error", (err) => {
